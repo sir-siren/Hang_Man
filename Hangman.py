fd5807d39821ddc6b7b1eb69557882374b49dbc6
Siren <78279514+Foster12@users.noreply.github.com>
2022-09-22 02:55:56 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,5 +1,6 @@
 import random
 
+#Update the word list to use the 'word_list' from hangman_words.py
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
@@ -8,6 +9,7 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
+#Import the logo from hangman_art.py and print it at the start of the game.
 from hangman_art import logo
 print(logo)
 
@@ -21,22 +23,21 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    
-#If the user has entered a letter they've already guessed, print the letter and let them know.
 
+    #If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
-            print(f"You've already guessed {guess}")
+        print(f"You've already guessed {guess}")
 
-        #Check guessed letter
-        for position in range(word_length):
-            letter = chosen_word[position]
-            #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-            if letter == guess:
-                display[position] = letter
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
 
-        #Check if user is wrong.
-        if guess not in chosen_word:
-            #If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        #If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
         
         lives -= 1
@@ -51,6 +52,8 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win.")
-        #Import the stages from hangman_art.py and make this error go away.
+
+    #Import the stages from hangman_art.py and make this error go away.
     from hangman_art import stages
-    print(stages[lives]
+    print(stages[lives])
+    
