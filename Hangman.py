1d2d03b1068594b1614109a28f64f819bcc91c05
Sir.Siren <78279514+sir-siren@users.noreply.github.com>
2023-01-03 22:48:20 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -11,10 +11,11 @@ lives = 6
 
 #Import the logo from hangman_art.py and print it at the start of the game.
 from hangman_art import logo
+
 print(logo)
 
 #Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+#print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
@@ -39,11 +40,11 @@ while not end_of_game:
     if guess not in chosen_word:
         #If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
-        
+
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print("You lose.")
+            print(f"You lose, The Answer is {chosen_word}.")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
@@ -56,4 +57,3 @@ while not end_of_game:
     #Import the stages from hangman_art.py and make this error go away.
     from hangman_art import stages
     print(stages[lives])
-    
